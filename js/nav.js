d8de985e3d68274f77f86d314a783734a7a399e3
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-11-04 12:20:16 +0330

    ✅ added jquery ✅ all js codes rewritten with jquery

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,9 @@
-// setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
 // show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+navbar = $('#navbar');
+$('#nav-btn').click(() => {
+	navbar.addClass('showNav');
 });
 // close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+$('#nav-close').click(() => {
+	navbar.removeClass('showNav');
 });
